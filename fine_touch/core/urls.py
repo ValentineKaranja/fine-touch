10616cd9cd8757a094f558bfd53bf54d31a9e202
IanMaguithi <maguithian13427@gmail.com>
2020-07-13 09:16:31 +0300

    fidjango templates working

--- fine_touch/core/urls.py
+++ fine_touch/core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('store/', views.store, name='store'),
+    path('order/', views.order, name='order'),
+    path('services/', views.services, name='services'),
+    path('product/', views.product, name='product'),
 ]
--- fine_touch/core/views.py
+++ fine_touch/core/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def product(request):
+    return render(request, 'prods.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+def order(request):
+    return render(request, 'order.html')
+
+
+def store(request):
+    return render(request, 'store.html')
